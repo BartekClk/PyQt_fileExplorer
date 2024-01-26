244050ea1583689693a07a131faa968414970a36
Bartek <clkbartek@gmail.com>
2024-01-26 02:35:23 +0100

    new file:   Example photos/mountain_lake.jpg 	new file:   Example photos/mountains.jpg 	new file:   Example photos/sunset_forest.jpg 	new file:   Example photos/sunset_mountain.jpg 	deleted:    left_arrow.png 	deleted:    left_arrow.svg 	modified:   python.py 	deleted:    right_arrow.png 	deleted:    right_arrow.svg 	modified:   untitled.ui

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QFileDialog, QMainWindow
-from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow
+from PyQt6.QtGui import QPixmap
 from PyQt6 import uic
+from PyQt6.QtCore import Qt
 import sys
 import os
 
@@ -10,35 +11,76 @@ class File_dialog(QMainWindow):
         uic.loadUi("untitled.ui", self)
         self.ui = uic.loadUi("untitled.ui", self)
 
-        left_arrow = self.ui.left_arrow
-        left_arrow.setStyleSheet("background-color: ; border: none;")
-        left_arrow.setIcon(QIcon("./left-arrow.png"))
-        left_arrow.setIconSize(left_arrow.size())
+        self.left_arrow = self.ui.left_arrow
+        self.right_arrow = self.ui.right_arrow
+        self.label = self.ui.image
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.openFile = self.ui.actionOpen
+        self.openFolder = self.ui.actionOpen_folder
+        self.images_path = []
+        self.actual_image = 0
+        self.status_bar = self.ui.statusbar
+        self.status_bar.showMessage("Otwórz zdjęcie")
 
-        # self.ui.open.clicked.connect(self.showFile)
+        self.openFile.triggered.connect(lambda: self.open_file())
+
+        self.left_arrow.setEnabled(False)
+        self.right_arrow.setEnabled(False)
+
+        self.right_arrow.clicked.connect(lambda: self.scrollRight())
+        self.left_arrow.clicked.connect(lambda: self.scrollLeft())
 
         self.show()
 
-    # def openFile(self):
-    #     filename, ok = QFileDialog.getOpenFileName(self, "Wybierz plik", "", "Images (*.png *.jpg *.jpeg)")
-    #     if filename:
-    #         path = os.path.abspath(filename)
-    #         self.ui.lineEdit.setText(path)
+    def open_file(self):
+        filename, ok = QFileDialog.getOpenFileName(self, "Wybierz plik", "", "Images (*.png *.jpg *.jpeg *.svg)")
+        
+        if filename:
+            image_path = os.path.abspath(filename)
+            folder_path = os.path.dirname(image_path)
+            
+            self.images_path = []
 
-    # def showFile(self):
-    #     path = self.ui.lineEdit.text()
-    #     flags = os.O_RDWR | os.O_CREAT
-    #     mode = 0o666
-    #     if path != "":
-    #         os.open(path, flags, mode)
+            for file in os.listdir(folder_path):
+                if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".svg"):
+                    self.images_path.append(os.path.join(folder_path, file))
+            self.load_image(self.images_path[0])
+            if len(self.images_path) > 1:
+                self.left_arrow.setEnabled(True)
+                self.right_arrow.setEnabled(True)
+            else:
+                self.left_arrow.setEnabled(False)
+                self.right_arrow.setEnabled(False)
+            self.updateStatusBar()
 
-    # def zaloguj(self):
-    #     login = self.ui.login.text()
-    #     password = self.ui.password.text()
-    #     if login == "admin" and password == "admin":
-    #         print("Zalogowano")
-    #     else:
-    #         print("Błędny login lub hasło")
+    def load_image(self, file_path):
+        pixmap = QPixmap(file_path)
+        pixmap_ratio = pixmap.width()/pixmap.height()
+        if pixmap_ratio == 1:
+            self.label.setPixmap(pixmap.scaled(self.label.width(), self.label.height(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+        elif pixmap_ratio > 1:
+            self.label.setPixmap(pixmap.scaledToWidth(self.label.width(), Qt.TransformationMode.SmoothTransformation))
+        else:
+            self.label.setPixmap(pixmap.scaledToHeight(self.label.height(), Qt.TransformationMode.SmoothTransformation))
+
+    def scrollRight(self):
+        if self.actual_image == len(self.images_path)-1:
+            self.actual_image = 0
+        else:
+            self.actual_image += 1
+        self.load_image(self.images_path[self.actual_image])
+        self.updateStatusBar()
+
+    def scrollLeft(self):
+        if self.actual_image == 0:
+            self.actual_image = len(self.images_path)-1
+        else:
+            self.actual_image -= 1
+        self.load_image(self.images_path[self.actual_image])
+        self.updateStatusBar()
+
+    def updateStatusBar(self):
+        self.status_bar.showMessage("Zdjęcie "+str(self.actual_image+1)+"/"+str(len(self.images_path))+"   "+os.path.basename(self.images_path[self.actual_image]))
 
 app=QApplication(sys.argv)
 file_dialog=File_dialog()
